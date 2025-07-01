3d00f1e289bbee3d6b31e42225357ebf84494e65
EdsonDanielMartinezGodinez <128575625+EdsonDanielMartinezGodinez@users.noreply.github.com>
2025-06-30 19:56:41 -0600

    Se hizo la interfaz grafica desmostrativa de l oque se espera del proyecto

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(3000, () => {
+  console.log(`Servidor corriendo en http://localhost:3000`);
+});
